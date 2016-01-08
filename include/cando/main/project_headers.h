c993d68335b5732e51ec8943ff6cdc0a84f5e7a9
Christian Schafmeister <chris.schaf@verizon.net>
2016-01-07 17:57:52 -0800

    added includes to project_headers.h

--- include/cando/main/project_headers.h
+++ include/cando/main/project_headers.h
@@ -1,11 +1,35 @@
-#ifdef GC_INTERFACE_FORWARD
-namespace chem {
-  class ExtractFragment_O;
-};
 
-#endif
+#include <llvm/ExecutionEngine/GenericValue.h>
+#include <llvm/ExecutionEngine/SectionMemoryManager.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/CodeGen/LinkAllCodegenComponents.h>
+#include <llvm/Bitcode/ReaderWriter.h>
+#include <llvm/Support/raw_ostream.h>
+#include <llvm/Support/MemoryBuffer.h>
+#include <llvm/Support/FileSystem.h>
+#include <llvm/ADT/Triple.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/DerivedTypes.h>
+#include <llvm/IR/Constants.h>
+#include <llvm/IR/GlobalVariable.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/CallingConv.h>
+#include <llvm/IR/BasicBlock.h>
+#include <llvm/IR/Instructions.h>
+#include <llvm/Transforms/Instrumentation.h>
+#include <llvm/Transforms/IPO.h>
+#include <llvm/IR/InlineAsm.h>
+#include <llvm/Support/FormattedStream.h>
+#include <llvm/Support/TargetRegistry.h>
+#include <llvm/Support/MathExtras.h>
+#include <llvm/Pass.h>
+#include <llvm/PassManager.h>
+#include <llvm/ADT/SmallVector.h>
+#include <llvm/IR/Verifier.h>
+#include "llvm/IR/AssemblyAnnotationWriter.h" // will be llvm/IR
 
-#ifdef GC_INTERFACE_INCLUDE
+
+
 #include <cando/adapt/adaptPackage.fwd.h>
 #include <cando/adapt/adaptPackage.h>
 #include <cando/adapt/adapters.h>
@@ -265,5 +289,3 @@ namespace chem {
 #include <cando/units/unit.h>
 #include <cando/units/unitsFoundation.h>
 #include <cando/units/unitsPackage.h>
-
-#endif
