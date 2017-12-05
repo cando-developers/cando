269dd0983a16a4b3d33c662ace21f59bf2125f70
Shiho Nagai <shiho.nagai@temple.edu>
2017-12-05 16:57:50 -0500

    Added a bunch of quickloads to get build to work

--- src/lisp/build-cando-jupyter.lisp
+++ src/lisp/build-cando-jupyter.lisp
@@ -1,3 +1,6 @@
 
 (load (merge-pathnames #P"build-cando.lisp" *load-pathname*))
+(ql:quickload "cl-jupyter")
+(ql:quickload "nglview")
+(ql:quickload "trivial-http")
 (asdf:load-asd (merge-pathnames #P"build-cando-jupyter.asd" *load-pathname*))
