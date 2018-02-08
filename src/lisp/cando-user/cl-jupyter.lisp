025e05b495d6862a2ce7dd19d4efbcb3a058bdd3
drmeister <chris.schaf@verizon.net>
2018-02-08 08:51:43 -0500

    add amber pathname translation

--- src/lisp/cando-user/cl-jupyter.lisp
+++ src/lisp/cando-user/cl-jupyter.lisp
@@ -1,6 +1,12 @@
 (in-package :cando-user)
 
 (defun cl-jupyter-kernel-start ()
+  (let ((amber-home
+          (namestring (or (uiop:ensure-directory-pathname (ext:getenv "AMBERHOME"))
+                          (uiop:ensure-directory-pathname "/home/app/amber16-data/")))))
+    (setf (logical-pathname-translations "amber")
+          (list (list "**;*.*" (concatenate 'string amber-home "/**/*.*"))))
+    (format t "Setting amber host pathname translation -> ~a~%" amber-home))
   (let ((*readtable* (copy-readtable)))
     (set-macro-character #\~ #'leap.core:leap-lookup-variable-reader-macro)
     (let ((cl-jup (find-symbol "KERNEL-START" "CL-JUPYTER")))
