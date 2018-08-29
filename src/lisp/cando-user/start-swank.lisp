508b1b5e6aadcbec5a6f7274ed0d805c7dd6a18b
Chris Schafmeister <meister@temple.edu>
2018-08-28 23:58:45 -0400

    Change start-swank so that it works
    with the precompiled slime

--- src/lisp/cando-user/start-swank.lisp
+++ src/lisp/cando-user/start-swank.lisp
@@ -6,20 +6,23 @@
     ;; Bad!  This is hard-coded to work with docker
     (if *started-swank*
         (format t "Swank is already running~%")
-        (progn
-          (funcall (find-symbol "SAVE-JUPYTER-CELL-STATE" :cl-ipywidgets))
-          (let* ((slime-home (cond
-                               ((and (ext:getenv "SLIME_HOME") (probe-file (pathname (ext:getenv "SLIME_HOME")))))
-                               ((probe-file #P"/home/app/slime/"))
-                               (t "Could not determine directory for slime - set SLIME_HOME")))
-                 (swank-loader (probe-file (merge-pathnames "swank-loader.lisp" slime-home))))
-            ;; (format t "swank-loader -> ~a~%" swank-loader)
-            (load swank-loader))
-          (let ((swank-loader-init (find-symbol "INIT" "SWANK-LOADER")))
-            (funcall swank-loader-init :delete nil :reload nil :load-contribs nil))
-          (let ((swank-create-server (find-symbol "CREATE-SERVER" "SWANK")))
-            (mp:process-run-function 'swank-main
-                                     (lambda () (funcall swank-create-server
-                                                         :port port
-                                                         :interface "0.0.0.0")))
-            (setf *started-swank* t))))))
+        (let ((save-jupyter-cell-state-symbol (find-symbol "SAVE-JUPYTER-CELL-STATE" :cl-ipywidgets)))
+          (if save-jupyter-cell-state-symbol
+              (funcall save-jupyter-cell-state-symbol)
+              (error "cl-ipywidgets isn't loaded - can't save cell state"))
+          (let* ((slime-home (and (ext:getenv "SLIME_HOME") (probe-file (pathname (ext:getenv "SLIME_HOME"))))))
+            (if slime-home
+                (let ((swank-loader (probe-file (merge-pathnames "swank-loader.lisp" slime-home))))
+                  (load swank-loader)
+                  (let* ((slime-cache (merge-pathnames "fasl/" slime-home))
+                         (slime-cache-symbol (find-symbol "*FASL-DIRECTORY*" :SWANK-LOADER)))
+                    (setf (symbol-value slime-cache-symbol) slime-cache)
+                    (let ((swank-loader-init (find-symbol "INIT" "SWANK-LOADER")))
+                      (funcall swank-loader-init :delete nil :reload nil :load-contribs nil))
+                    (let ((swank-create-server (find-symbol "CREATE-SERVER" "SWANK")))
+                      (mp:process-run-function 'swank-main
+                                               (lambda () (funcall swank-create-server
+                                                                   :port port
+                                                                   :interface "0.0.0.0")))
+                      (setf *started-swank* t))))
+                (error "Could not determine directory for slime - set SLIME_HOME")))))))
