a7e0606d51cc626d184d5a2c0061b62a0a8dba3b
Chris Schafmeister <meister@temple.edu>
2019-01-02 19:38:12 -0500

    Add end-of-line in add-ions

--- src/lisp/leap/add-ions.lisp
+++ src/lisp/leap/add-ions.lisp
@@ -33,7 +33,7 @@
     (if (and (= 0 target-charge)
              (= 0 ion1-number))
         (progn
-          (format t "The total charge is ~a and so there are no charges to neutralize." target-charge)
+          (format t "The total charge is ~a and so there are no charges to neutralize.~%" target-charge)
           (return-from add-ions))
         (format t "Total charge ~f~%" target-charge))
     ;;Consider neutralizetion    
