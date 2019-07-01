fdf1ce66361dcd52fff8bcb921ca2c6bae6c4126
Chris Schafmeister <meister@temple.edu>
2019-06-30 20:32:41 -0400

    Fix parallel-build-cando.lisp

--- src/lisp/parallel-build-cando.lisp
+++ src/lisp/parallel-build-cando.lisp
@@ -1,6 +1,22 @@
-(load "~/quicklisp/setup.lisp")
+(load "quicklisp:setup.lisp")
 
 (defparameter *number-of-jobs* 8)
+
+;;;#+(or)
+(progn
+  (defparameter *log* (open "/tmp/clasp-builder-log.txt" :direction :output :if-exists :supersede))
+  (si:fset 'mmsg #'(lambda (whole env)
+                           (let ((fmt (cadr whole))
+                                 (args (cddr whole)))
+                             `(progn
+                                (core:bformat *log* ,fmt ,@args)
+                                (finish-output *log*))))
+           t))
+
+#+(or)
+(si:fset 'mmsg #'(lambda (whole env)
+                         nil)
+         t)
 
 
 (defun optimize-tree (levels)
@@ -56,6 +72,30 @@
     levels))
 
 
+
+(defstruct pjob done pid signals entry counter child-stdout child-stderr)
+
+
+(defun wait-for-child-to-exit (jobs)
+  (mmsg "About to waitpid sigchld-count: %s%N"(core:sigchld-count))
+  (multiple-value-bind (wpid status)
+      (core:wait)
+    (mmsg "Returned from waitpid with wpid: %s status:%s%N" wpid status)
+    (if (not (= wpid 0))
+        (progn
+          (unless (= status 0)
+            (core:bformat t "wpid -> %s  status -> %s%N" wpid status))
+          (when (core:wifexited status)
+            (mmsg "A child exited wpid: %s  status: %s%N" wpid status)
+            (return-from wait-for-child-to-exit (values wpid status nil)))
+          (when (core:wifsignaled status)
+            (let ((signal (core:wtermsig status)))
+              (mmsg "Child process with pid %s got signal %s%N" wpid signal)
+              (warn "Child process with pid ~a got signal ~a" wpid signal)))))
+    ;; If we drop through to here the child died for some reason - return and inform the parent
+    (values wpid status t)))
+
+
 (defun quickload-parallel (systems level &key (parallel-jobs *number-of-jobs*))
   #+dbg-print(bformat t "DBG-PRINT compile-system files: %s%N" files)
   (let ((total (length systems))
@@ -64,11 +104,8 @@
         (child-count 0)
         (jobs (make-hash-table :test #'eql)))
     (labels ((started-quickload (entry counter child-pid)
-               (let* ((filename entry))
-                 (format t "Compiling [level ~d/~d of ~d (child-pid: ~d)] ~a~%~%" level counter total child-pid filename)))
-             (finished-report-one (entry counter child-pid)
-               (let* ((filename entry))
-                 (format t "Finished [level ~d/~d of ~d (child pid: ~d)] system '~a' - output follows...~%" level counter total child-pid filename)))
+               (let ((filename entry))
+                 (format t "quickloading [level ~d/~d of ~d (child-pid: ~d)] ~a~%~%" level counter total child-pid filename)))
              (report-result-stream (result-stream child-pid)
                (when result-stream ;;;can be empty
                  (with-open-stream (sin result-stream)
@@ -79,65 +116,120 @@
                      (write-string "]--> ")
                      (princ line)
                      (terpri)))))
-             (finished-one (entry counter child-pid result-stream)
-               (finished-report-one entry counter child-pid)
-               (report-result-stream result-stream child-pid))
+             (finished-report-one (job entry child-pid)
+               (let ((filename entry))
+                 (format t "Finished [~a of ~d (child pid: ~d)] ~a output follows...~%" (pjob-counter job) total child-pid filename)))
+             (read-fd-into-buffer (fd)
+               (let ((buffer (make-array 1024 :element-type 'base-char :adjustable nil))
+                     (sout (make-string-output-stream)))
+                 (core:lseek fd 0 :seek-set)
+                 (block readloop
+                   (tagbody
+                    top
+                      (mmsg "About to read-fd%N")
+                      (multiple-value-bind (num-read errno)
+                          (core:read-fd fd buffer)
+                        (when (= num-read 0)
+                          (return-from readloop))
+                        (when (< num-read 0)
+                          (mmsg "Three was an error reading the stream errno %d%N" errno)
+                          (core:bformat t "There was an error reading the stream errno %d%N" errno))
+                        (when (> num-read 0)
+                          (write-sequence buffer sout :start 0 :end num-read)
+                          (mmsg "Wrote <%s>%N" (subseq buffer 0 num-read))))
+                      (go top)))
+                 (mmsg "Returning with buffer%N")
+                 (core:close-fd fd)
+                 (get-output-stream-string sout)))
+             (report-stream (pid fd name)
+               (mmsg "In report-stream%N")
+               (let* ((buffer (read-fd-into-buffer fd))
+                      (sin (make-string-input-stream buffer)))
+                 (do ((line (read-line sin nil nil) (read-line sin nil nil)))
+                     ((null line))
+                   (core:bformat t "--%d(%s)--> %s%N" pid name line)
+                   (finish-output))))
+             (report-child-exited (child-pid child-stdout child-stderr)
+               (mmsg "In report-child-exited: %s %s%N" child-stdout child-stderr)
+               (report-stream child-pid child-stdout "out")
+               (report-stream child-pid child-stderr "err"))
+             (finished-some (child-pid pjob)
+               (let* ((entry (pjob-entry pjob))
+                      (child-stdout (pjob-child-stdout pjob))
+                      (child-stderr (pjob-child-stderr pjob)))
+                 (setf (pjob-done pjob) t)
+                 (finished-report-one pjob entry child-pid)
+                 (report-child-exited child-pid child-stdout child-stderr)))
              (one-quickload (entry counter)
-               (ql:quickload entry :verbose t)))  ;; Yeah silent and verbose
+               (ql:quickload entry :verbose t))) ;; Yeah silent and verbose
       (let (entry wpid status)
         (tagbody
          top
-           (incf counter)
-           (setq entry (pop systems))
-           (incf job-counter)
-           (when (> job-counter parallel-jobs)
-             (block wait-loop
-               (tagbody
-                top
-                  (multiple-value-setq (wpid status) (core:wait))
-                  (core:bformat t "wpid -> %s  status -> %s%N" wpid status)
-                  (when (core:wifexited status) (go done))
-                  (when (core:wifsignaled status)
-                    (let ((signal (core:wtermsig status)))
-                      (warn "Child process with pid ~a got signal ~a" signal)))
-                  (go top)
-                done
-                  ))
-             (if (>= wpid 0)
-                 (let* ((finished-entries-triplet (gethash wpid jobs))
-                        (finished-entries (first finished-entries-triplet))
-                        (finished-counter (second finished-entries-triplet))
-                        (result-stream (third finished-entries-triplet)))
-                   (finished-one finished-entries finished-counter wpid result-stream)
-                   (decf child-count))
-                 (error "wait returned ~d  status ~d~%" wpid status)))
-           (when entry
-             (multiple-value-bind (maybe-error pid-or-error result-stream)
-                 (core:fork t)
-               (if maybe-error
-                   (error "Could not fork when trying to build ~a" entry)
-                   (let ((pid pid-or-error))
-                     (if (= pid 0)
-                         (progn
-                           ;; Turn off interactive mode so that errors cause clasp to die with backtrace
-                           (core:set-interactive-lisp nil)
-                           (let ((new-sigset (core:make-cxx-object 'core:sigset))
-                                 (old-sigset (core:make-cxx-object 'core:sigset)))
-                             (core:sigset-sigaddset new-sigset 'core:signal-sigint)
-                             (core:sigset-sigaddset new-sigset 'core:signal-sigchld)
-                             (multiple-value-bind (fail errno)
-                                 (core:sigthreadmask :sig-setmask new-sigset old-sigset)
-                               (one-quickload entry counter)
-                               (core:sigthreadmask :sig-setmask old-sigset nil)
-                               (when fail
-                                 (error "sigthreadmask has an error errno = ~a" errno))
-                               (core:exit))))
-                         (progn
-                           (started-quickload entry counter pid)
-                           (setf (gethash pid jobs) (list entry counter result-stream))
-                           (incf child-count)))))))
+           (when (or (and (null systems) (> child-count 0))
+                     (> job-counter parallel-jobs))
+             (multiple-value-setq (wpid status child-died)
+               (wait-for-child-to-exit jobs))
+             (if (null child-died)
+                 (if (and (numberp wpid) (>= wpid 0))
+                     (let* ((pjob (gethash wpid jobs))
+                            (finished-entry (pjob-entry pjob)))
+                       (finished-some wpid pjob)
+                       (when (and (numberp status)
+                                  (not (zerop status)))
+                         (format *error-output* "wait returned for process ~d status ~d: exiting compile-system~%" wpid status)
+                         (core:exit 1))
+                       (decf child-count))
+                     (error "wait returned ~d  status ~d~%" wpid status))
+                 (if (and (numberp wpid) (>= wpid 0))
+                     (progn
+                       (finished-some wpid (gethash wpid jobs))
+                       (error "The child with wpid ~a died with status ~a - terminating build" wpid status)
+                       (core:exit 1))
+                     (progn
+                       (error "A child died with wpid ~a status ~a" wpid status)
+                       (core:exit 1)))))
+           (when systems
+             (incf counter)
+             (setq entry (pop systems))
+             (incf job-counter)
+             (let ((child-stdout (core:mkstemp-fd "cando-build-stdout"))
+                   (child-stderr (core:mkstemp-fd "cando-build-stderr")))
+               (multiple-value-bind (maybe-error pid-or-error result-stream)
+                   (core:fork-redirect child-stdout child-stderr)
+                 (if maybe-error
+                     (error "Could not fork when trying to build ~a" entry)
+                     (let ((pid pid-or-error))
+                       (if (= pid 0)
+                           (progn
+                             ;; Turn off interactive mode so that errors cause clasp to die with backtrace
+                             (core:set-interactive-lisp nil)
+                             (let ((new-sigset (core:make-cxx-object 'core:sigset))
+                                   (old-sigset (core:make-cxx-object 'core:sigset)))
+                               (core:sigset-sigaddset new-sigset 'core:signal-sigint)
+                               (core:sigset-sigaddset new-sigset 'core:signal-sigchld)
+                               (multiple-value-bind (fail errno)
+                                   (core:sigthreadmask :sig-setmask new-sigset old-sigset)
+                                 (one-quickload entry counter)
+                                 (core:sigthreadmask :sig-setmask old-sigset nil)
+                                 (when fail
+                                   (error "sigthreadmask has an error errno = ~a" errno))
+                                 (finish-output)
+                                 (sleep 1)
+                                 (core:exit))))
+                           (let ((pjob (make-pjob
+                                        :done nil
+                                        :pid pid
+                                        :signals nil
+                                        :entry entry
+                                        :counter counter
+                                        :child-stdout child-stdout
+                                        :child-stderr child-stderr)))
+                             (started-quickload entry counter pid)
+                             (setf (gethash pid jobs) pjob)
+                             (incf child-count))))))))
            (when (> child-count 0) (go top))))))
-  (format t "Leaving compile-system-parallel~%"))
+  (format t "Leaving quickload-parallel~%"))
+
 
 
 ;;; ------------------------------------------------------------
@@ -188,35 +280,35 @@
 
 
 (defparameter *load-systems*
-  (("cando-user") ("leap-commands" "leap" "design" "charges" "cando")
-   ("architecture.builder-protocol" "parser.common-rules.operators"
-                                    "parser.common-rules" "esrap" "alexandria" "smirnoff" "amber" "charges"
-                                    "leap" "cando" "aromaticity" "smarts" "inet" "utility")
-   ("let-plus" "split-sequence" "plump" "cl-ppcre" "aromaticity" "alexandria"
-               "smirnoff" "cando" "inet" "utility" "smarts" "architecture.builder-protocol"
-               "parser.common-rules.operators" "parser.common-rules" "language.smarts"
-               "esrap" "trivial-http")
-   ("anaphora" "documentation-utils" "array-utils" "plump" "cando" "aromaticity"
-               "smarts" "inet" "utility" "trivial-http" "language.smarts" "let-plus"
-               "split-sequence" "language.smiles" "architecture.builder-protocol"
-               "parser.common-rules.operators" "parser.common-rules" "esrap" "alexandria"
-               "usocket")
-   ("trivial-indent" "documentation-utils" "array-utils" "inet" "utility"
-                     "smarts" "language.smarts" "trivial-http" "usocket" "language.smiles"
-                     "parser.common-rules.operators" "anaphora" "parser.common-rules"
-                     "architecture.builder-protocol" "esrap" "let-plus" "alexandria"
-                     "split-sequence")
-   ("trivial-indent" "trivial-http" "language.smarts" "language.smiles"
-                     "parser.common-rules.operators" "usocket" "parser.common-rules"
-                     "architecture.builder-protocol" "esrap" "let-plus" "split-sequence"
-                     "anaphora" "alexandria")
-   ("usocket" "language.smiles" "parser.common-rules.operators"
-              "parser.common-rules" "architecture.builder-protocol" "esrap" "let-plus"
-              "split-sequence" "anaphora" "alexandria")
-   ("parser.common-rules" "architecture.builder-protocol" "esrap" "let-plus"
-                          "split-sequence" "anaphora" "alexandria")
-   ("esrap" "let-plus" "split-sequence" "anaphora" "alexandria")
-   ("anaphora" "alexandria")))
+  #(("cando-user") ("leap-commands" "leap" "design" "charges" "cando")
+    ("architecture.builder-protocol" "parser.common-rules.operators"
+     "parser.common-rules" "esrap" "alexandria" "smirnoff" "amber" "charges"
+     "leap" "cando" "aromaticity" "smarts" "inet" "utility")
+    ("let-plus" "split-sequence" "plump" "cl-ppcre" "aromaticity" "alexandria"
+     "smirnoff" "cando" "inet" "utility" "smarts" "architecture.builder-protocol"
+     "parser.common-rules.operators" "parser.common-rules" "language.smarts"
+     "esrap" "trivial-http")
+    ("anaphora" "documentation-utils" "array-utils" "plump" "cando" "aromaticity"
+     "smarts" "inet" "utility" "trivial-http" "language.smarts" "let-plus"
+     "split-sequence" "language.smiles" "architecture.builder-protocol"
+     "parser.common-rules.operators" "parser.common-rules" "esrap" "alexandria"
+     "usocket")
+    ("trivial-indent" "documentation-utils" "array-utils" "inet" "utility"
+     "smarts" "language.smarts" "trivial-http" "usocket" "language.smiles"
+     "parser.common-rules.operators" "anaphora" "parser.common-rules"
+     "architecture.builder-protocol" "esrap" "let-plus" "alexandria"
+     "split-sequence")
+    ("trivial-indent" "trivial-http" "language.smarts" "language.smiles"
+     "parser.common-rules.operators" "usocket" "parser.common-rules"
+     "architecture.builder-protocol" "esrap" "let-plus" "split-sequence"
+     "anaphora" "alexandria")
+    ("usocket" "language.smiles" "parser.common-rules.operators"
+     "parser.common-rules" "architecture.builder-protocol" "esrap" "let-plus"
+     "split-sequence" "anaphora" "alexandria")
+    ("parser.common-rules" "architecture.builder-protocol" "esrap" "let-plus"
+     "split-sequence" "anaphora" "alexandria")
+    ("esrap" "let-plus" "split-sequence" "anaphora" "alexandria")
+    ("anaphora" "alexandria")))
 
 (defun quickload-calculate-order ()
   "Calculate the order of systems to build"
