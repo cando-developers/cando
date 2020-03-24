e898fa53f55a819eea407a35de3b4736af52bea9
su-chris <su-chris@cst11668.fios-router.home>
2020-03-23 21:55:31 -0400

    Add regression-tests run-all.lisp for cando

---
+++ src/lisp/regression-tests/run-all.lisp
@@ -0,0 +1,10 @@
+(in-package :cl-user)
+
+(declaim (optimize (safety 3)))
+
+(load (compile-file "sys:regression-tests;framework.lisp"))
+
+(in-package :clasp-tests)
+
+(reset-clasp-tests)
+(load-if-compiled-correctly "source-dir:extensions;cando;src;lisp;regression-tests;spanning-tree.lisp")
