84d90fefd0542a636016d5d244e4ae1261039ac8
Chris Schafmeister <meister@temple.edu>
2020-05-23 02:06:51 -0400

    A few more fixes to the calculate-masks algorithm
    There were a few bugs that caused hydrogens to be missed.

--- src/lisp/tirun/tirun-from-structures.lisp
+++ src/lisp/tirun/tirun-from-structures.lisp
@@ -36,17 +36,19 @@
   (let (heavy-match heavy-diff hydrogens)
     (loop for bond in (chem:bonds-as-list atm)
           for other-atm = (chem:get-other-atom bond atm)
-          do (if (= (chem:atomic-number-for-element (chem:get-element other-atm)) 1)
-                 (push atm hydrogens)
+          for element = (chem:get-element other-atm)
+          for atomic-number = (chem:atomic-number-for-element element)
+          do (if (= atomic-number 1)
+                 (push other-atm hydrogens)
                  (if (member atm diffs)
-                     (push atm heavy-diff)
-                     (push atm heavy-match))))
+                     (push other-atm heavy-diff)
+                     (push other-atm heavy-match))))
     (values heavy-match heavy-diff hydrogens)))
 
 (defun find-closest-hydrogen (atm hydrogens)
   (let* ((closest-hydrogen (first hydrogens))
          (closest-distance-squared (chem:distance-squared-between-two-atoms atm closest-hydrogen)))
-    (loop for hyd in (second hydrogens)
+    (loop for hyd in (cdr hydrogens)
           for dist-squared = (chem:distance-squared-between-two-atoms atm hyd)
           when (< dist-squared closest-distance-squared)
             do (setf closest-distance-squared dist-squared
@@ -58,7 +60,8 @@
 
 
 (defun match-hydrogens (hydrogens1 hydrogens2)
-  (let ((equivs nil))
+  (let ((equivs nil)
+        hyd2)
     (loop for hyd1 in hydrogens1
           do (multiple-value-setq (hyd2 hydrogens2)
                (find-closest-hydrogen hyd1 hydrogens2))
@@ -95,45 +98,58 @@
                           (setf equivs (push-all (match-hydrogens hydrogen1 hydrogen2) equivs)))
                          ;; Otherwise the hydrogens are all considered different
                          (t
-                          (setf diff1 (push-all hydrogens1 diff1)
-                                diff2 (push-all hydrogens2 diff2))))))))
+                          (setf diff1 (push-all hydrogen1 diff1)
+                                diff2 (push-all hydrogen2 diff2))))))))
+      (loop for heavy1 in heavy-diff1
+            do (multiple-value-bind (heavy-match heavy-diff hydrogens)
+                   (bonded-atoms heavy1 heavy-diff1)
+                 (setf diff1 (push-all hydrogens diff1))))
+      (loop for heavy2 in heavy-diff2
+            do (multiple-value-bind (heavy-match heavy-diff hydrogens)
+                   (bonded-atoms heavy2 heavy-diff2)
+                 (setf diff2 (push-all hydrogens diff2))))
       (values equivs diff1 diff2))))
-                              
+
+(defun calculate-masks-for-molecules (source-molecule target-molecule)
+  "Calculate the mask using the max-clique algorithm to find the largest common subgraph
+   and what remains of each molecule are the differences used to construct the mask"
+  (multiple-value-bind (heavy-equivs heavy-diff1 heavy-diff2)
+      (molecule-graph.max-clique:compare-molecules
+       source-molecule
+       target-molecule
+       :atom-match-callback (lambda (atom1 atom2)
+                              (chem:atom-within-angstroms atom1 atom2 0.3))
+       :exclude-hydrogens t)
+    (integrate-hydrogens-into-comparison heavy-equivs heavy-diff1 heavy-diff2)))
+
 (defmethod calculate-masks (source target (method (eql :closest)))
   "Calculate the mask using the max-clique algorithm to find the largest common subgraph
    and what remains of each molecule are the differences used to construct the mask"
   (let ((source-molecule (molecule source))
         (target-molecule (molecule target)))
-    (multiple-value-bind (heavy-equivs heavy-diff1 heavy-diff2)
-        (molecule-graph.max-clique:compare-molecules
-         source-molecule
-         target-molecule
-         :atom-match-callback (lambda (atom1 atom2)
-                                (chem:atom-within-angstroms atom1 atom2 0.3))
-         :exclude-hydrogens t)
-      (multiple-value-bind (equivs diff1 diff2)
-          (integrate-hydrogens-into-comparison heavy-equivs heavy-diff1 heavy-diff2)
-        (unless (= (chem:content-size source-molecule) 1)
-          (error "The source molecule ~a has more than one residue"))
-        (unless (= (chem:content-size target-molecule) 1)
-          (error "The target molecule ~a has more than one residue"))
-        (let ((source-atom-names (mapcar 'chem:get-name diff1))
-              (target-atom-names (mapcar 'chem:get-name diff2)))
-          (make-instance 'ti-mask
-                         :equivalent-atom-names (let (pairs)
-                                                  (loop for pair in equivs
-                                                        do (let ((source-atom (car pair))
-                                                                 (target-atom (cdr pair)))
-                                                             (let ((source-name (chem:get-name source-atom))
-                                                                   (target-name (chem:get-name target-atom)))
-                                                               (push (cons source-name target-name) pairs))))
-                                                  pairs)
-                         :source source
-                         :source-timask-residue-index 1
-                         :source-scmask-atom-names source-atom-names
-                         :target target
-                         :target-timask-residue-index 2
-                         :target-scmask-atom-names target-atom-names))))))
+    (multiple-value-bind (equivs diff1 diff2)
+        (calculate-masks-for-molecules source-molecule target-molecule)
+      (unless (= (chem:content-size source-molecule) 1)
+        (error "The source molecule ~a has more than one residue"))
+      (unless (= (chem:content-size target-molecule) 1)
+        (error "The target molecule ~a has more than one residue"))
+      (let ((source-atom-names (mapcar 'chem:get-name diff1))
+            (target-atom-names (mapcar 'chem:get-name diff2)))
+        (make-instance 'ti-mask
+                       :equivalent-atom-names (let (pairs)
+                                                (loop for pair in equivs
+                                                      do (let ((source-atom (car pair))
+                                                               (target-atom (cdr pair)))
+                                                           (let ((source-name (chem:get-name source-atom))
+                                                                 (target-name (chem:get-name target-atom)))
+                                                             (push (cons source-name target-name) pairs))))
+                                                pairs)
+                       :source source
+                       :source-timask-residue-index 1
+                       :source-scmask-atom-names source-atom-names
+                       :target target
+                       :target-timask-residue-index 2
+                       :target-scmask-atom-names target-atom-names)))))
 
 
 
