b385cf65750d5f2a99cc002df79ea9446a5a0246
Chris Schafmeister <meister@temple.edu>
2020-06-06 21:12:48 -0400

    Update the tirun-jupyter.lisp demo

--- src/lisp/tirun-jupyter/tirun-jupyter.lisp
+++ src/lisp/tirun-jupyter/tirun-jupyter.lisp
@@ -1,13 +1,37 @@
 (in-package :tirun-jupyter)
 
+(defparameter *fu* nil)
 
 (defun setup-receptor ()
+  (format t "setup-receptor ~%")
   (let* ((box-layout (make-instance 'w:layout :width "auto" :flex-flow "row wrap"))
          (desc-width "12em")
-         (desc-style (make-instance 'w:description-style :description-width desc-width)))
-    (make-instance 'w:h-box
+         (desc-style (make-instance 'w:description-style :description-width desc-width))
+         (fu (make-instance 'jupyter-widgets:file-upload :description "Upload PDB file"))
+         (nglview (make-instance 'nglv:nglwidget)))
+    (setf *fu* fu)
+    (jupyter-widgets:observe fu :data
+                             (lambda (instance type name old-value new-value source)
+                               (declare (ignore instance type name old-value source))
+                               (format t "Value changed~%")
+                               (format t "old-value lengths -> ~a~%" (mapcar #'length old-value))
+                               (format t "new-value lengths -> ~a~%" (mapcar #'length new-value))
+                               (when new-value
+                                 (let* ((last-pdb (car (last new-value)))
+                                        (octets (make-array (length last-pdb) :element-type 'ext:byte8
+                                                                              :initial-contents last-pdb)))
+                                   (let ((as-text (babel:octets-to-string octets)))
+                                     (dolist (id (nglv::ngl-component-ids nglview)) (nglv::remove-component nglview id))
+                                     (nglv::add-structure nglview (make-instance 'nglv::TextStructure :text as-text)))))))
+    (make-instance 'w:v-box
                    :layout box-layout
-                   :children (list (make-instance 'w:file-upload :description "Upload PDB File" :style desc-style)))))
+                   :children (list
+                              (make-instance 'w:h-box
+                                             :layout box-layout
+                                             :children (list fu))
+                              (make-instance 'w:h-box
+                                             :layout box-layout
+                                             :children (list nglview))))))
 
 
 #|
