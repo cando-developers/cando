b1a8e7994703fb0eba1e693a570b8e4f12440c69
Tarn W. Burton <twburton@gmail.com>
2022-05-19 20:27:30 -0400

    Remove unneeded files

--- src/cando-startup.lisp
+++
@@ -1,8 +0,0 @@
-(format t "Entered cando-startup.lisp~%")
-(push :dont-start-cando-user *features*)
-(load "sys:extensions;cando;src;lisp;start-cando.lisp")
-(format t "Done load start-cando.lisp in cando-startup.lisp~%")
-
-(push (lambda ()
-        (ql:quickload :cando-user))
-      core:*extension-startup-evals*)
--- src/lisp/workbench.lisp
+++
@@ -1,8 +0,0 @@
-
-(require :asdf)
-(push (translate-logical-pathname #P"cando:cando;") asdf:*central-registry*)
-(asdf:load-system :cando)
-
-(defparameter *s* #(1 2 3 4))
-(nreverse *s*)
-*s*
