e117e585b01357ad585091e02776102cf99e3892
Christian Schafmeister <chris.schaf@verizon.net>
2022-11-19 12:16:56 -0500

    More additions to topology module

---
+++ src/lisp/topology/conformation.lisp
@@ -0,0 +1,71 @@
+(in-package :topology)
+
+(defclass conformation ()
+  ((aggregate :initarg :aggregate :accessor aggregate)
+   (ataggregate :initarg :ataggregate :accessor ataggregate)
+   (joint-tree :initarg :joint-tree :accessor joint-tree)))
+
+(defun make-conformation (oligomer)
+  (multiple-value-bind (aggregate molecule monomer-positions)
+      (topology:build-molecule oligomer)
+    (let* ((ataggregate (make-instance 'ataggregate :aggregate aggregate))
+           (joint-tree (make-joint-tree)))
+      (resize-atmolecules ataggregate 1)
+      (format t "monomer-positions: ~a~%" (alexandria:hash-table-alist monomer-positions))
+      (let ((molecule-index 0))
+        ;; This is where I would invoke Conformation_O::buildMoleculeUsingOligomer
+        ;; Use the monomers-to-topologys
+        (let* ((atmolecule (build-atmolecule-using-oligomer oligomer molecule molecule-index monomer-positions joint-tree)))
+          (put-atmolecule ataggregate atmolecule molecule-index)
+          (let ((conf (make-instance 'conformation
+                                     :aggregate aggregate
+                                     :ataggregate ataggregate
+                                     :joint-tree joint-tree)))
+            conf
+            ))))))
+
+(defun walk-atoms-joints (conformation callback)
+  (let ((aggregate (aggregate conformation))
+        (ataggregate (ataggregate conformation)))
+    (loop for molecule-index below (chem:content-size aggregate)
+          for molecule = (chem:content-at aggregate molecule-index)
+          for atmolecule = (aref (atmolecules ataggregate) molecule-index)
+          do (loop for residue-index below (chem:content-size molecule)
+                   for residue = (chem:content-at molecule residue-index)
+                   for atresidue = (aref (atresidues atmolecule) residue-index)
+                   do (loop for atom-index below (chem:content-size residue)
+                           for atom = (chem:content-at residue atom-index)
+                           for joint = (aref (joints atresidue) atom-index)
+                           do (funcall callback atom joint (list molecule-index residue-index atom-index)))))))
+
+(defun copy-atom-positions-into-joints (conformation)
+  (walk-atoms-joints conformation
+                     (lambda (atm jnt atomid)
+                       (declare (ignore atomid))
+                       (kin:set-position jnt (chem:get-position atm)))))
+
+(defun copy-joint-positions-into-atoms (conformation)
+  (walk-atoms-joints conformation
+                     (lambda (atm jnt atomid)
+                       (declare (ignore atomid))
+                       (chem:set-position atm (kin:get-position jnt)))))
+
+(defun update-joint-tree-internal-coordinates (conformation)
+  (let ((ataggregate (ataggregate conformation)))
+    (walk-ataggregate-joints ataggregate
+                             (lambda (joint atom-id)
+                               (declare (ignore atom-id))
+                               (kin:update-internal-coord joint)))))
+  
+(defun build-all-atom-tree-external-coordinates (conformation)
+  (let ((joint (root (joint-tree conformation))))
+    (kin:update-xyz-coords joint)))
+
+
+(defun zero-all-atom-tree-external-coordinates (conf)
+  "Set the external coordinates for each joint to the origin"
+  (let ((ataggregate (ataggregate conf)))
+    (walk-ataggregate-joints ataggregate
+                             (lambda (joint atom-id)
+                               (declare (ignore atom-id))
+                               (kin:set-position joint (geom:vec 0.0 0.0 0.0))))))
---
+++ src/lisp/topology/fold.lisp
@@ -0,0 +1,216 @@
+(in-package :topology)
+
+(defclass atmatter () (()))
+
+(defclass ataggregate (atmatter)
+  ((aggregate :initarg :aggregate :accessor aggregate)
+   (atmolecules :initarg :atmolecules :initform (make-array 0 :adjustable t) :accessor atmolecules)))
+
+(defun put-atmolecule (ataggregate atmolecule index)
+  (check-type ataggregate ataggregate)
+  (check-type atmolecule atmolecule)
+  (setf (aref (atmolecules ataggregate) index) atmolecule))
+
+(defmethod resize-atmolecules ((ataggregate ataggregate) num-atmolecules)
+  (adjust-array (atmolecules ataggregate) num-atmolecules))
+
+
+(defun walk-ataggregate-joints (ataggregate callback)
+  (loop for molecule-index below (length (atmolecules ataggregate))
+        for atmolecule = (aref (atmolecules ataggregate) molecule-index)
+        do (loop for residue-index below (length (atresidues atmolecule))
+                 for atresidue = (aref (atresidues atmolecule) residue-index)
+                 do (loop for atom-index below (length (joints atresidue))
+                          for joint = (aref (joints atresidue) atom-index)
+                          do (funcall callback joint (list molecule-index residue-index atom-index))))))
+
+(defclass atmolecule (atmatter)
+  ((molecule :initarg :molecule :accessor molecule)
+   (root-atresidue :initarg :root-atresidue :accessor root-atresidue)
+   (atresidues :initarg :atresidues
+               :initform (make-array 0 :adjustable t)
+               :accessor atresidues)))
+
+(defmethod resize-atresidues ((atmolecule atmolecule) num-atresidues)
+  (adjust-array (atresidues atmolecule) num-atresidues))
+
+(defun put-atresidue (atmolecule atresidue index)
+  (check-type atmolecule atmolecule)
+  (check-type atresidue atresidue)
+  (setf (aref (atresidues atmolecule) index) atresidue))
+
+(defun atresidue-factory (residue ring-closing-monomer-map monomer topology)
+  (cond
+    ((has-ring-closing-coupling monomer)
+     (let ((atresidue (make-instance 'ring-closing-atresidue :residue residue)))
+       (setf (gethash ring-closing-monomer-map monomer atresidue) atresidue)
+       atresidue))
+    (t (make-instance 'atresidue :residue residue
+                                 :topology topology))))
+
+(defun lookup-atresidue-id (atmolecule atresidue-id)
+  (aref (atresidues atmolecule) atresidue-id))
+
+(defun make-ring-closing-connections (ring-closing-monomer-map)
+  (when (> (hash-table-count ring-closing-monomer-map) 0)
+    (error "Implement me")))
+             
+
+(defun build-atmolecule-using-oligomer (oligomer molecule molecule-index monomer-positions joint-tree)
+  (let* ((root-monomer (root-monomer oligomer))
+         (ring-closing-monomer-map (make-hash-table))
+         (atmolecule (make-instance 'atmolecule :molecule molecule))
+         (residue-index (gethash root-monomer monomer-positions))
+         (residue (chem:content-at molecule residue-index))
+         (topology (get-current-topology root-monomer))
+         (root-atresidue (atresidue-factory residue ring-closing-monomer-map root-monomer topology)))
+    (adjust-array (atresidues atmolecule) (number-of-monomers oligomer))
+    (put-atresidue atmolecule root-atresidue residue-index)
+    (recursively-build-children joint-tree
+                                root-atresidue
+                                atmolecule
+                                molecule
+                                ring-closing-monomer-map
+                                nil
+                                nil
+                                root-monomer
+                                monomer-positions
+                                molecule-index
+                                residue-index
+                                nil
+                                )
+    (make-ring-closing-connections ring-closing-monomer-map)
+    (setf (root-atresidue atmolecule) root-atresidue)
+    atmolecule))
+
+(defclass atresidue (atmatter)
+  ((residue :initarg :residue :accessor residue)
+   (parent :initarg :parent :initform nil :accessor parent)
+   (parent-plug-name :initarg :parent-plug-name :initform nil :accessor parent-plug-name)
+   (id :initarg :id :accessor id)
+   (children :type hash-table :initform (make-hash-table) :initarg :children :accessor children)
+   (stereoisomer-name :initform nil :initarg :stereoisomer-name :accessor stereoisomer-name)
+   (topology :initarg :topology :accessor topology)
+   (conformation-index :initarg :conformation-index :accessor conformation-index)
+   (joints :initarg :joints
+                   :initform (make-array 0 :adjustable t)
+                   :accessor joints)))
+
+(defun put-joint (atresidue joint index)
+  (check-type atresidue atresidue)
+  (check-type joint kin:joint)
+  (setf (aref (joints atresidue) index) joint))
+
+
+
+(defmethod resize-atatoms ((atresidue atresidue) num-atatoms)
+  (adjust-array (joints atresidue) num-atatoms))
+
+
+(defclass ring-closing-atresidue (atresidue)
+  ())
+
+(defun recursively-build-children (joint-tree
+                                   atresidue
+                                   atmolecule
+                                   molecule
+                                   ring-closing-monomer-map
+                                   parent-atresidue
+                                   coupling
+                                   monomer
+                                   monomer-positions
+                                   atmolecule-index
+                                   atresidue-index
+                                   parent-joint)
+  "Recursively build a atmolecule from an oligomer by linking together kin:atresidues"
+  (when parent-atresidue
+    (setf (parent atresidue) parent-atresidue))
+  (when coupling
+    (setf (parent-plug-name atresidue) (target-plug-name coupling)))
+  (let ((outgoing-plug-names-to-joint-map (fill-atresidue joint-tree atresidue parent-joint atmolecule-index atresidue-index))
+        (current-topology (get-current-topology monomer)))
+    (setf (stereoisomer-name atresidue) (current-stereoisomer-name monomer)
+          (topology atresidue) current-topology
+          (conformation-index atresidue) 0)
+    (maphash (lambda (plug-name coupling)
+               (declare (ignore plug-name))
+               (unless (typep coupling 'ring-coupling)
+                 (let ((directional-coupling coupling))
+                   (when (eq (source-monomer directional-coupling) monomer)
+                     (let* ((other-monomer (target-monomer directional-coupling))
+                            (other-topology (get-current-topology other-monomer))
+                            (other-residue-index (gethash other-monomer monomer-positions))
+                            (other-residue (chem:content-at molecule other-residue-index))
+                            (other-atresidue (atresidue-factory other-residue
+                                                                ring-closing-monomer-map
+                                                                other-monomer
+                                                                other-topology))
+                            (out-plug-name (source-plug-name coupling)))
+                       (put-atresidue atmolecule other-atresidue other-residue-index)
+                       (setf (gethash out-plug-name (children atresidue)) other-atresidue)
+                       (let ((new-parent-joint (gethash out-plug-name outgoing-plug-names-to-joint-map)))
+                         (recursively-build-children joint-tree
+                                                     other-atresidue
+                                                     atmolecule
+                                                     molecule
+                                                     ring-closing-monomer-map
+                                                     atresidue
+                                                     directional-coupling
+                                                     other-monomer
+                                                     monomer-positions
+                                                     atmolecule-index
+                                                     other-residue-index
+                                                     new-parent-joint
+                                                     )))))))
+             (couplings monomer))))
+
+(defun describe-recursively (atresidue prefix stream)
+  (princ prefix stream)
+  (when (parent-plug-name atresidue)
+    (princ (parent-plug-name atresidue) stream)
+    (princ " " stream))
+  (format stream "~a[~a]" (class-name (class-of atresidue)) (stereoisomer-name atresidue))
+  (loop for child-atresidue across (children atresidue)
+        do (describe-recursively child-atresidue (concatenate 'string prefix "  ") stream)))
+
+
+(defmethod print-object ((object atresidue) stream)
+  (print-unreadable-object (object stream :type t)
+    (format stream "~a[~a]" (class-name (class-of object)) (stereoisomer-name object))))
+
+(defun add-joint (atresidue index joint)
+  (when (< (length (joints atresidue)) index)
+    (adjust-array (joints atresidue) (1+ index)))
+  (setf (aref (joints atresidue) index) joint))
+
+(defun walk-joints (atresidue function)
+  (loop for joint across (joints atresidue)
+        for joint-index from 0
+        do (funcall function joint-index joint)))
+
+
+
+#+(or)(eval-when (:compile-toplevel :execute :load-toplevel)
+  (export
+   '(
+     atmatter
+     ataggregate
+     atmolecules
+     atmolecule
+     root-atresidue
+     atresidues
+     atresidue
+     parent
+     parent-plug-name
+     id
+     children
+     stereoisomer-name
+     topology
+     conformation-index
+     joints
+     put-joint
+     walk-ataggregate-joints
+     update-internal-coordinates
+     zero-external-coordinates
+     ))
+  )
