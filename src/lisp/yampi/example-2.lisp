0f838b57d1eb0e5d3dd5b8364af10f530ed0f397
Tarn W. Burton <twburton@gmail.com>
2024-04-14 12:26:33 -0400

    Add connect/disconnect logic

--- src/lisp/yampi/example-2.lisp
+++ src/lisp/yampi/example-2.lisp
@@ -9,17 +9,21 @@
   ((remaining-jobs :accessor remaining-jobs
                    :initform (loop for i below 30
                                    collect i))
+   (clients :accessor clients
+            :initform (make-hash-table :test #'equalp))
    (pending-jobs :accessor pending-jobs
                  :initform (make-hash-table))))
 
 (defmethod yampi:receive ((channel server) identity (code (eql :request)) &rest parts)
   (declare (ignore parts))
   (with-accessors ((remaining-jobs remaining-jobs)
+                   (clients clients)
                    (pending-jobs pending-jobs))
       channel
     (cond (remaining-jobs
            (let ((job (pop remaining-jobs)))
-             (setf (gethash job pending-jobs) nil)
+             (setf (gethash job pending-jobs) nil
+                   (gethash identity clients) (cons job (gethash identity clients)))
              (yampi:send channel identity :assign
                          (format nil "~a" job))))
           ((zerop (hash-table-count pending-jobs))
@@ -30,8 +34,10 @@
 
 (defmethod yampi:receive ((channel server) identity (code (eql :done)) &rest parts)
   (with-accessors ((remaining-jobs remaining-jobs)
+                   (clients clients)
                    (pending-jobs pending-jobs))
       channel
+    (setf (gethash identity clients) (remove (car parts) (gethash identity clients)))
     (remhash (car parts) pending-jobs)
     (format *debug-io* "Job done ~2d, ~2d remaining, ~2d pending.~%"
             (car parts) (length remaining-jobs) (hash-table-count pending-jobs))
@@ -43,20 +49,43 @@
 
 (defmethod yampi:receive ((channel server) identity (code (eql :fail)) &rest parts)
   (with-accessors ((remaining-jobs remaining-jobs)
+                   (clients clients)
                    (pending-jobs pending-jobs))
       channel
+    (setf (gethash identity clients) (remove (car parts) (gethash identity clients)))
     (remhash (car parts) pending-jobs)
     (format *debug-io* "Job fail ~2d, ~2d remaining, ~2d pending.~%"
             (car parts) (length remaining-jobs) (hash-table-count pending-jobs))
     (push (car parts) remaining-jobs)))
 
-(defun make-server (connection-path)
-  (let ((channel (make-instance 'server))
-        (endpoint "tcp://0.0.0.0:*" (machine-instance)))
+(defmethod yampi:receive ((channel server) identity (code (eql :connect)) &rest parts)
+  (declare (ignore parts))
+  (format *debug-io* "Client ~a has connected.~%" identity))
+
+(defmethod yampi:receive ((channel server) identity (code (eql :disconnect)) &rest parts)
+  (declare (ignore parts))
+  (with-accessors ((remaining-jobs remaining-jobs)
+                   (clients clients)
+                   (pending-jobs pending-jobs))
+      channel
+    (format *debug-io* "Client ~a has disconnected.~%" identity)
+    (dolist (job (gethash identity clients))
+      (remhash job pending-jobs)
+      (push job remaining-jobs))
+    (remhash identity clients)))
+
+(defun make-server (connection-path
+                    &key (endpoint (format nil "tcp://~a:*" (machine-instance))))
+  (let ((channel (make-instance 'server)))
     (yampi:start channel connection-path
                  :threaded nil
                  :control-endpoint endpoint
-                 :broadcast-endpoint endpoint)
+                 :broadcast-endpoint endpoint
+                 :hello-msg (symbol-name :connect)
+                 :disconnect-msg (symbol-name :disconnect)
+                 :heartbeat-ivl 1000
+                 :heartbeat-ttl 5000
+                 :heartbeat-timeout 5000)
     channel))
 
 (defclass client (yampi:client)
@@ -64,14 +93,15 @@
                    :initform (lparallel:make-channel))))
 
 (defmethod yampi:receive ((channel client) identity (code (eql :assign)) &rest parts)
+  (declare (ignore identity))
   (lparallel:submit-task (kernel-channel channel)
-                         (lambda (id &aux (status :fail))
+                         (lambda (id)
                            (sleep (random 20))
                            (list (if (zerop (random 10)) :fail :done) id))
                          (car parts)))
 
 (defmethod yampi:receive ((channel client) identity (code (eql :stop)) &rest parts)
-  (declare (ignore parts))
+  (declare (ignore identity parts))
   (yampi:stop channel))
 
 (defmethod yampi:idle ((channel client))
@@ -84,7 +114,10 @@
 
 (defun make-client (connection-path &key threaded)
   (let ((channel (make-instance 'client)))
-    (yampi:start channel connection-path :threaded threaded)
+    (yampi:start channel connection-path
+                 :threaded threaded
+                 :hello-msg (symbol-name :connect)
+                 :disconnect-msg (symbol-name :disconnect))
     channel))
 
 (defmethod yampi:initialize ((channel client))
@@ -93,14 +126,18 @@
         do (yampi:send channel nil :request)))
 
 (defmethod yampi:receive (channel identity code &rest parts)
-  (format *debug-io* "~&No handler for code ~a~%" code))
+  (declare (ignore channel identity parts))
+  (format *debug-io* "~&No handler for code ~s~%" code))
 
 (defmethod yampi:deserialize (channel (position (eql 0)) (part string))
+  (declare (ignore channel))
   (or (find-symbol part :keyword)
       part))
 
 (defmethod yampi:deserialize (channel (position (eql 1)) (part string))
+  (declare (ignore channel))
   (read-from-string part nil nil))
 
 (defmethod yampi:serialize (channel (position (eql 1)) (part number))
+  (declare (ignore channel))
   (write-to-string part))
