1a9b704ac49b51b20c05599ffe5618ebce59232b
meister <meister@thirdlaw.tech>
2024-11-16 12:07:36 -0500

    Add align-transform

--- src/lisp/cando/align.lisp
+++ src/lisp/cando/align.lisp
@@ -4,3 +4,7 @@
 (defun align-atoms (&key moveable-atoms fixed-atoms moveable-matter)
   "Align the atoms in MOVEABLE-ATOMS-SEQUENCE to the FIXED-ATOMS-SEQUENCE and apply the transformation to MOVEABLE-MATTER (an aggregate, molecule, or residue)."
   (chem:align-matter moveable-matter moveable-atoms fixed-atoms))
+
+(defun align-transform (&key moveable-atoms fixed-atoms)
+  "Align the atoms in MOVEABLE-ATOMS-SEQUENCE to the FIXED-ATOMS-SEQUENCE and return the transform"
+  (chem:align-transform moveable-atoms fixed-atoms))
